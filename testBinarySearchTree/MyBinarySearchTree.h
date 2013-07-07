0b8dc8905c77c929cc036078786fddbde899ec41
Xinlei Cao <lukecxl@gmail.com>
2013-07-07 19:23:14 -0400

    set aside

--- testBinarySearchTree/MyBinarySearchTree.h
+++ testBinarySearchTree/MyBinarySearchTree.h
@@ -29,7 +29,9 @@ public:
 	~BinarySearchTree(){}
 
 	bool InOrderTraverse_Recursion(BinaryTreeNode<Data> *node, bool (* Visit)( Data e));
-	bool InOrderTraverse_Stack(BinaryTreeNode<Data> *node, bool (* Visit)( Data e));
+	/*bool InOrderTraverse_Stack(BinaryTreeNode<Data> *node, bool (* Visit)( Data e));*/
+
+	bool Search()
 };
 
 template <class Data> bool BinarySearchTree<Data>::InOrderTraverse_Recursion(BinaryTreeNode<Data> *node, bool (* Visit)( Data e)){
@@ -42,25 +44,25 @@ template <class Data> bool BinarySearchTree<Data>::InOrderTraverse_Recursion(Bin
 	}else
 		return true;
 }
-
-template <class Data> bool BinarySearchTree<Data>::InOrderTraverse_Stack(BinaryTreeNode<Data> *node, bool (* Visit)( Data e)){
-	Stack<BinaryTreeNode *> mStack;
-	while(node||!mStack.IsEmpty()){
-		if(node){
-			if(!mStack.Push(node))
-				return false;
-			node = node->left;
-		}else{
-			BinaryTreeNode *tempNode = nullptr;
-			if(!mStack.Pop(tempNode))
-				return false;
-			if(!Visit(node->mData))
-				return false;
-			node = tempNode->right;
-		}
-	}
-	return true;
-}
+//
+//template <class Data> bool BinarySearchTree<Data>::InOrderTraverse_Stack(BinaryTreeNode<Data> *node, bool (* Visit)( Data e)){
+//	Stack<BinaryTreeNode *> mStack;
+//	while(node||!mStack.IsEmpty()){
+//		if(node){
+//			if(!mStack.Push(node))
+//				return false;
+//			node = node->left;
+//		}else{
+//			BinaryTreeNode *tempNode = nullptr;
+//			if(!mStack.Pop(tempNode))
+//				return false;
+//			if(!Visit(node->mData))
+//				return false;
+//			node = tempNode->right;
+//		}
+//	}
+//	return true;
+//}
 
 // =============================================
 #endif // !__MY_BINARY_SEARCH_TREE__
